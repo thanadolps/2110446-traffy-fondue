5d23dd0d23a00b6050a81c361b0fe0a18ad4db60
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-05-14 19:48:36 +0700

    ✨ Image downloader

---
+++ traffy_train/urls_download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl --config ./data/traffy_photo_urls.txt --parallel --parallel-immediate 
+
+# --parallel-max 100
+# –parallel-max
+# --output-dir ./data/traffy_photoss/
+# --parallel --parallel-immediate --parallel-max 3
